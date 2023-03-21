56fa35b4c26a12153491cf2b759411b389f58829
NEO xD <122731828+neolol3@users.noreply.github.com>
2023-03-21 10:36:09 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
-    TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
-    TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
+    TELEGRAM_TOKEN=os.environ['5753023029:AAEUUDBCaFlqRqzHsTckm4wuMBhbuAEL-t4']
+    TELEGRAM_APP_HASH=os.environ['80c20ae3166c904aa2137523564d46df']
+    TELEGRAM_APP_ID=int(os.environ['14029604'])
     
     if not TELEGRAM_TOKEN:
         raise ValueError('TELEGRAM BOT TOKEN not set')
